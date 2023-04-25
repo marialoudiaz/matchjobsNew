61c3601503bac8e36e3d9037d2739d0cde2f6d81
marialoudiaz <loumariadiaz@gmail.com>
2023-04-25 09:17:57 +0200

    controller

--- client/src/Views/Welcome.js
+++ client/src/Views/Welcome.js
@@ -1,6 +1,7 @@
 import App from '../App.css'
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+
 function Welcome() {
   let navigate = useNavigate();
 
@@ -8,11 +9,17 @@ function Welcome() {
 
   return (
     <>
-<h1>Welcome to the new era of recruitement</h1>
-<h3>matchjobs</h3>
-<p>are you looking for a job application or a job offer ?</p>
-<button onClick={() => navigate("/login")}>Login</button>
-<button onClick={() => navigate("/register")}>Register</button>
+<div className='WP-Container'>
+    <div className='WP-text'>
+      <h1>Welcome to the new era of recruitement</h1>
+      <h3>matchjobs</h3>
+      <p>are you looking for a job application or a job offer ?</p>
+  </div>
+<div className='WP-btn'> 
+  <button className='btn' onClick={() => navigate("/login")}>Login</button>
+  <button className='btn' onClick={() => navigate("/register")}>Register</button>
+</div>     
+</div>
  </>
   )
 }
