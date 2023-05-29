950eba19bc8bb37244d2f4512608bad3df73f181
marialoudiaz <loumariadiaz@gmail.com>
2023-05-29 17:39:31 +0200

    css

--- client/src/Views/Welcome.js
+++ client/src/Views/Welcome.js
@@ -11,15 +11,22 @@ function Welcome() {
   return (
     <>
   <div className='WP-Container'>
+    <div className='WP-Header'>
+      <div>logo</div>
+      <div>log in</div>
+      <div>get started</div>
+    </div>
       <div className='WP-text'>
         <h1>Welcome to the new era of recruitement</h1>
-        <h3>matchjobs</h3>
-        <p>Find a job application or a job offer</p>
-      </div>
-  <div> 
-  <button className='btnWP' onClick={() => navigate("/login")}>Login</button>
-  <button className='btnWP' onClick={() => navigate("/register")}>Register</button>
-</div>     
+        <h2>search for skills. find people</h2>
+        <h3>matchjobs</h3> {/*logo */}
+        <div className='WP-log'>
+        <div> 
+          <button className='btnWP' onClick={() => navigate("/login")}>Login</button>
+          <button className='btnWP' onClick={() => navigate("/register")}>Register</button>
+        </div>    
+        </div>
+      </div> 
 </div>
  </>
   )
